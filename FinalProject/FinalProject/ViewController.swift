926e409861f1e535a2526f0f8d12dd287966400e
Jakob Daugherty <jadppf@mail.missouri.edu>
2017-07-26 21:33:05 -0500

    UI built and functioning
    The UI is now built and has some functionality

--- FinalProject/FinalProject/ViewController.swift
+++ FinalProject/FinalProject/ViewController.swift
@@ -9,9 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    enum Conversion {
+        case FtoC, CtoF, MtoKm, KmtoM
+    }
+    
+    var conversion: Conversion = Conversion.FtoC
 
+    @IBOutlet weak var outputLabel: UITextField!
+    @IBOutlet weak var inputLabel: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if conversion == Conversion.FtoC {
+            inputLabel.text = "F"
+            outputLabel.text = "C"
+        }
+        if conversion == Conversion.CtoF {
+            inputLabel.text = "C"
+            outputLabel.text = "F"
+        }
+        if conversion == Conversion.MtoKm {
+            inputLabel.text = "M"
+            outputLabel.text = "Km"
+        }
+        if conversion == Conversion.KmtoM {
+            inputLabel.text = "Km"
+            outputLabel.text = "M"
+        }
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +47,34 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func getConversion(_ sender: Any) {
+        let alert = UIAlertController(title: "Choice", message: "Choose Conveter", preferredStyle: UIAlertControllerStyle.actionSheet)
+        alert.addAction(UIAlertAction(title: "fahrenheit to celcius", style: UIAlertActionStyle.default, handler: {
+            (alertAction) -> Void in
+            // handle fahrenheit to celcius
+            self.conversion = Conversion.FtoC
+            self.viewDidLoad()
+        }))
+        alert.addAction(UIAlertAction(title: "celcius to fahrenheit", style: UIAlertActionStyle.default, handler: {
+            (alertAction) -> Void in
+            // handle celcius to fahrenheit
+            self.conversion = Conversion.CtoF
+            self.viewDidLoad()
+        }))
+        alert.addAction(UIAlertAction(title: "miles to kilometers", style: UIAlertActionStyle.default, handler: {
+            (alertAction) -> Void in
+            // handle celcius to fahrenheit
+            self.conversion = Conversion.MtoKm
+            self.viewDidLoad()
+        }))
+        alert.addAction(UIAlertAction(title: "kilometers to miles", style: UIAlertActionStyle.default, handler: {
+            (alertAction) -> Void in
+            // handle celcius to fahrenheit
+            self.conversion = Conversion.KmtoM
+            self.viewDidLoad()
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 }
 
