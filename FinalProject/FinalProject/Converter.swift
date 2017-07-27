be33874abbcf9a1107ef6cf9198d94514d6d8003
Jakob Daugherty <jadppf@mail.missouri.edu>
2017-07-27 12:39:08 -0500

    building up I/O logic

--- FinalProject/FinalProject/Converter.swift
+++ FinalProject/FinalProject/Converter.swift
@@ -13,38 +13,78 @@ class Converter: NSObject {
         case FtoC, CtoF, MtoKm, KmtoM
     }
     var conversion: Conversion = Conversion.FtoC
-    var outputText: String = "°C"
-    var inputText: String = "°F"
+    var inputSuffix: String = "°F"
+    var outputSuffix: String = "°C"
+    var outputText: String = ""
+    var inputText: String = ""
+    var rawInput: String = ""
+    
     
     override init() {
         self.conversion = Conversion.FtoC
+        self.inputText = rawInput + inputSuffix
+        self.outputText = rawInput + outputSuffix
+        
+    }
+    
+    func convert() {
+        self.inputText = rawInput + "" + inputSuffix
+        self.outputText = rawInput + "" + outputSuffix
+        
+    }
+    
+    func uiHandle(newValue: String) {
+        //handle UI values and add to raw input
+        if newValue == "-" {
+            if self.rawInput.contains("-") {
+                self.rawInput.remove(at: self.rawInput.startIndex)
+            } else {
+                let temp: String = newValue + self.rawInput
+                self.rawInput = temp
+            }
+            
+        } else if newValue == "." {
+            if self.rawInput.contains(".") {
+                //do nothing
+            } else {
+                self.rawInput += newValue
+            }
+        } else if newValue == "C" {
+            self.rawInput = ""
+        } else {
+            self.rawInput += newValue
+        }
+        self.convert()
+        
+        
     }
     
     func setConversion(newValue: Int){
         switch newValue {
         case 1:
             self.conversion = Conversion.FtoC
-            self.outputText = "°C"
-            self.inputText = "°F"
+            self.inputSuffix = "°F"
+            self.outputSuffix = "°C"
         case 2:
             self.conversion = Conversion.CtoF
-            self.outputText = "°F"
-            self.inputText = "°C"
+            self.inputSuffix = "°C"
+            self.outputSuffix = "°F"
         case 3:
             self.conversion = Conversion.MtoKm
-            self.outputText = "Km"
-            self.inputText = "M"
+            self.inputSuffix = " M"
+            self.outputSuffix = "Km"
         case 4:
             self.conversion = Conversion.KmtoM
-            self.outputText = "M"
-            self.inputText = "Km"
+            self.inputSuffix = "Km"
+            self.outputSuffix = " M"
         //default is to go to FtoC conversion
         default:
             self.conversion = Conversion.FtoC
-            self.outputText = "°C"
-            self.inputText = "°F"
+            self.inputSuffix = "°F"
+            self.outputSuffix = "°C"
             
         }
+        self.convert()
         
     }
 
--- FinalProject/FinalProject/ViewController.swift
+++ FinalProject/FinalProject/ViewController.swift
@@ -31,7 +31,88 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func handle9(_ sender: Any) {
+        //handle touch of 9
+        converter.uiHandle(newValue: "9")
+        viewDidLoad()
+    }
+    
+    @IBAction func handle8(_ sender: Any) {
+        //handle touch of 8
+        converter.uiHandle(newValue: "8")
+        viewDidLoad()
+    }
+    
+    @IBAction func handle7(_ sender: Any) {
+        //handle touch of 7
+        converter.uiHandle(newValue: "7")
+        viewDidLoad()
+    }
+    
+    @IBAction func handle6(_ sender: Any) {
+        //handle touch of 6
+        converter.uiHandle(newValue: "6")
+        viewDidLoad()
+    }
+    
+    @IBAction func handle5(_ sender: Any) {
+        //handle touch of 5
+        converter.uiHandle(newValue: "5")
+        viewDidLoad()
+    }
+    
+    @IBAction func handle4(_ sender: Any) {
+        //handle touch of 4
+        converter.uiHandle(newValue: "4")
+        viewDidLoad()
+    }
+    
+    @IBAction func handle3(_ sender: Any) {
+        //handle touch of 3
+        converter.uiHandle(newValue: "3")
+        viewDidLoad()
+    }
+    
+  
+    @IBAction func handle2(_ sender: Any) {
+        //handle touch of 2
+        converter.uiHandle(newValue: "2")
+        viewDidLoad()
+    }
+    
+    @IBAction func handle1(_ sender: Any) {
+        //handle touch of 1
+        converter.uiHandle(newValue: "1")
+        viewDidLoad()
+        
+    }
+    
+    @IBAction func handle0(_ sender: Any) {
+        //handle touch of 0
+        converter.uiHandle(newValue: "0")
+        viewDidLoad()
+    }
+    
+    @IBAction func handleDecimal(_ sender: Any) {
+        //handle touch of .
+        converter.uiHandle(newValue: ".")
+        viewDidLoad()
+    }
 
+    @IBAction func handleNegitive(_ sender: Any) {
+        //handle touch of +/- 
+        converter.uiHandle(newValue: "-")
+        viewDidLoad()
+        
+    }
+    @IBAction func handleC(_ sender: Any) {
+        //handle touch of clear 
+        converter.uiHandle(newValue: "C")
+        viewDidLoad()
+    }
+    
     @IBAction func getConversion(_ sender: Any) {
         let alert = UIAlertController(title: "Choice", message: "Choose Conveter", preferredStyle: UIAlertControllerStyle.actionSheet)
         alert.addAction(UIAlertAction(title: "fahrenheit to celcius", style: UIAlertActionStyle.default, handler: {
